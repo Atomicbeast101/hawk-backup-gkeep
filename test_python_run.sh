442d4a2fe1797e00b6fba80bf94b4691afa98fa6
adam <adam@brodack.net>
2025-11-15 12:00:08 -0600

    Delete test_python_run.sh

--- test_python_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Google Drive
-export GOOGLE_API_USERNAME=atomicbrod101@gmail.com
-export GOOGLE_API_SERVICE_ACCOUNT_FILE=./google_api_service_account.json
-
-# Run python script
-.venv/bin/python extract.py
